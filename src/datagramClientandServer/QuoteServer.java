4eb5d41d99f0357c55ab871007e760fca80c9ab4
sameow <sameow.ong@gmail.com>
2014-10-28 11:09:51 +0800

    Prototype codes working

---
+++ src/datagramClientandServer/QuoteServer.java
@@ -0,0 +1,9 @@
+package datagramClientandServer;
+
+import java.io.IOException;
+
+public class QuoteServer {
+	 public static void main(String[] args) throws IOException {
+	        new QuoteServerThread().start();
+	    }
+}
