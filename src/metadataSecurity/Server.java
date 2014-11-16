2d13e87957b4b9040fe877012d0a4258039b0ba3
Samuel Ong <Samuel Ong@SamuelOng-PC>
2014-11-17 07:14:25 +0800

    Commit weekend's work (15-16 Nov)

--- src/metadataSecurity/Server.java
+++ src/metadataSecurity/Server.java
@@ -2,15 +2,18 @@ package metadataSecurity;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketImpl;
+import java.net.UnknownHostException;
 
 public class Server {
 	public Server(){
@@ -38,7 +41,7 @@ public class Server {
 	            System.out.println(e.getMessage());
 	        }
 	}
-	public void splitFile(String fileName, int fileSize, Socket clientSocket) throws IOException{
+	private void splitFile(String fileName, int fileSize, Socket clientSocket) throws IOException{
 		File receivedFile = new File(fileName);
 		FileOutputStream fos = new FileOutputStream(receivedFile,true);
 		BufferedOutputStream bos = new BufferedOutputStream(fos);
@@ -48,11 +51,34 @@ public class Server {
 	    fos.flush();
 	    bos.close();
 	    fos.close();
-	    
+	  
 	   //fileSplit(receivedFile);
+	    sendSharesToOthers();
+	    
 	}
 	
-	public void combineFile(){
+	private void sendSharesToOthers() {
+		Socket socket;
+		PrintWriter output;
+		BufferedReader input;
+		 try {
+			 	socket = new Socket(server, 4444);
+			 	output = new PrintWriter(socket.getOutputStream(), true);
+			 	input = new BufferedReader(
+	                new InputStreamReader(socket.getInputStream()));
+	        } catch (UnknownHostException e) {
+	            System.err.println("Don't know about host " + server);
+	            System.exit(1);
+	        } catch (IOException e) {
+	            System.err.println("Couldn't get I/O for the connection to " +
+	                server);
+	            System.exit(1);
+	        }
+		output.println("Sending shares.");
+//		output.println(shares n relevant info);
+		
+	}
+	private void combineFile(){
 		
 	}
 	 public static void main(String[] args) throws IOException {
